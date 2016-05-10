7463fb4c28d894b59da6b3f6979b5070ee1a70fb
Rushil Basappa <rushil.oucse37@gmail.com>
2016-05-10 20:39:42 +0530

    seed data updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,23 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Rushil",
+             email: "rushirocks2212@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+User.create!(name:  "Sai Chander",
+             email: "saichander.ramaram@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
-users = User.all
-user = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+User.create!(name:  "Sashank",
+             email: "challasashank@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
