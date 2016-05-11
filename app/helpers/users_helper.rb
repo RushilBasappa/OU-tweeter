0604f7de13ae1eb6a36c95056d629e8b929a90b8
Rushil Basappa <rushil.oucse37@gmail.com>
2016-05-11 23:04:34 +0530

    Changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
   def gravatar_for(user,size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon&r=PG"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def gravatar_for_profile_card(user,size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon&r=PG"
     image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle img-thumbnail")
   end
 end
