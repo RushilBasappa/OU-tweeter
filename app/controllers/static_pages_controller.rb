29ae6c23c88fb5d4d7cce053ff9c90eddae30763
Rushil Basappa <rushil.oucse37@gmail.com>
2016-05-12 12:19:24 +0530

    Updated

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @admins = User.all.where(:admin => true)
+    @admins = User.all.where(:admin => true).order(:name)
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
