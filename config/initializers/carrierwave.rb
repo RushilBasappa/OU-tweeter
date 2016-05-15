538666a1a69505deeaa394783e779be0c0cc012d
Rushil Basappa <rushil.oucse37@gmail.com>
2016-05-16 00:24:16 +0530

    Updated

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:               'AWS',
+    provider:              'AWS',
+    #aws_access_key_id:    " AKIAI4NTIQHNKL5DVBOA" ,
     aws_access_key_id:      ENV["S3_KEY"],
+    #aws_secret_access_key: "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"
     aws_secret_access_key:  ENV["S3_SECRET"]
   }
   config.fog_directory  = 'osmtweet-images'
